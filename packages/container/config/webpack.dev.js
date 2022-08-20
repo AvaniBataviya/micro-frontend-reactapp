e219bd99f4fd3c9e484077a10a20beaa758c4e4d
Avani Bataviya <avanibataviya2610@gmail.com>
2022-08-20 17:49:07 +0530

    Added public path in container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
-
-const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require('./webpack.common');
 const packageJson = require("../package.json");
 
 const devConfig = {
@@ -19,9 +18,6 @@ const devConfig = {
         marketingApp: 'marketing@http://localhost:8081/remoteEntry.js',
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
