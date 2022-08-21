580ca874eea66713b356a13f1494baef92241ed9
Avani Bataviya <avanibataviya2610@gmail.com>
2022-08-21 19:29:37 +0530

    Added class generation logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,21 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Markrting from './components/Markrting';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix:'co'
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
-      <h3 style={{textAlign: 'center'}}>Hi there, welcome to container!</h3>
-      <Markrting/>
+      <StylesProvider generateClassName={generateClassName}>
+          <Header/>
+          <h3 style={{textAlign: 'center'}}>Hi there, welcome to container!</h3>
+          <Markrting/>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix:'ma'
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route path="/pricing" component={Pricing} />
