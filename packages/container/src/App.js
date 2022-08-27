03a61d4d3569f1e31e66cdb3f776d4978f1682ba
Avani Bataviya <avanibataviya2610@gmail.com>
2022-08-27 19:19:58 +0530

    Added lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,13 @@ import {
   createGenerateClassName,
   StylesProvider
 } from '@material-ui/core/styles';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import Markrting from './components/Markrting';
-import Auth from './components/Auth';
+import Progress from './components/Progress';
+
+const Markrting = lazy(() => import('./components/Markrting'));
+const Auth = lazy(() => import('./components/Auth'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -17,11 +19,12 @@ const App = () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <Header />
-        <h3 style={{ textAlign: 'center' }}>Hi there, welcome to container!</h3>
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/" component={Markrting} />
-        </Switch>
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/" component={Markrting} />
+          </Switch>
+        </Suspense>
       </StylesProvider>
     </BrowserRouter>
   );
